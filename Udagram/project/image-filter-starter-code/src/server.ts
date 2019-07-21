eee51524401f390b19863121361ec720c651b977
Yuri Gouveia <yuri.gouveia@eng.ci.ufpb.br>
2019-07-21 16:33:03 -0300

    Clean code

--- Udagram/project/image-filter-starter-code/src/server.ts
+++ Udagram/project/image-filter-starter-code/src/server.ts
@@ -13,46 +13,31 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-   //CORS Should be restricted
-   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
   app.get("/filteredimage/", async (req, res) => {
     let image_url = req.query.image_url;
 
-    if(!image_url) {
-      res.status(400).send('Image url required');
+    const imageValidator = String(image_url).includes('.png')  || String(image_url).includes('.jpg') ||
+                           String(image_url).includes('.jpeg') || String(image_url).includes('.bmp') && 
+                           !String(image_url).includes('http');
+
+    if(!imageValidator) {
+      res.status(400).send('Image url invalid');
     }
 
     const filteredImage = await filterImageFromURL(image_url);
     res.status(200).sendFile(filteredImage, err =>{
       if(err){
-        console.log(err);
+        res.status(404).send(err)
       }
       deleteLocalFiles([filteredImage]);
     });
   });
-
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
